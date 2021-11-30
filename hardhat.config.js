378ffda1f2cf6360d3a27cce3709b08b183af343
Pierfabio Sanna <pierfabiosanna7@gmail.com>
2021-11-30 22:35:40 +0100

    hardhat configuration improvement

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@typechain/hardhat");
+require("hardhat-deploy")
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
