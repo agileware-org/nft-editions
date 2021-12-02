db7cc3dd0174a4e58f6e37e8fe1f05814124ef07
Narthar <narthar@gmail.com>
2021-12-03 00:02:47 +0100

    [UPD] Added deploy typescript file

---
+++ scripts/deploy.ts
@@ -0,0 +1,41 @@
+import { ethers, upgrades } from "hardhat";
+
+async function deploy(contract) {
+  console.log("Deploying " + contract + " in progress...");
+  const factory = await ethers.getContractFactory(contract);
+  const instance = await factory.deploy();
+  await instance.deployed();
+  console.log(contract + " deployed to: ", instance.address);
+  return instance.address;
+}
+
+async function deployWithArgs(contract, args) {
+  console.log("Deploying " + contract + " in progress...")
+  const factory = await ethers.getContractFactory(contract);
+  const instance = await factory.deploy(args);
+  await instance.deployed();
+  console.log(contract + " deployed to: ", instance.address);
+  return instance.address;
+}
+async function deployWithProxy(contract) {
+  console.log("Deploying " + contract + " in progress...")
+  const factory = await ethers.getContractFactory(contract);
+  const proxy = await upgrades.deployProxy(factory, [], { initializer: false, kind: 'uups' });
+  await proxy.deployed();
+  console.log(contract + " deployed to: ", proxy.address);
+  return proxy.address;
+}
+
+async function main() {
+  metadata = await deploy("EditionMetadata");
+  editions = await deployWithArgs("MintableEditions", metadata);
+  factory = await deployWithArgs("MintableEditionsFactory", editions);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
