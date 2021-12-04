902f1bccbd8e1e860c0c0580c46190e891b729c8
Narthar <narthar@gmail.com>
2021-12-04 18:55:08 +0100

    [UPD] uploaded correct deploy.ts file

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,6 @@
 import { ethers, upgrades } from "hardhat";
 
-async function deploy(contract) {
+async function deploy(contract: string) {
   console.log("Deploying " + contract + " in progress...");
   const factory = await ethers.getContractFactory(contract);
   const instance = await factory.deploy();
@@ -9,7 +9,7 @@ async function deploy(contract) {
   return instance.address;
 }
 
-async function deployWithArgs(contract, args) {
+async function deployWithArgs(contract: string, args: string) {
   console.log("Deploying " + contract + " in progress...")
   const factory = await ethers.getContractFactory(contract);
   const instance = await factory.deploy(args);
@@ -17,7 +17,7 @@ async function deployWithArgs(contract, args) {
   console.log(contract + " deployed to: ", instance.address);
   return instance.address;
 }
-async function deployWithProxy(contract) {
+async function deployWithProxy(contract: string) {
   console.log("Deploying " + contract + " in progress...")
   const factory = await ethers.getContractFactory(contract);
   const proxy = await upgrades.deployProxy(factory, [], { initializer: false, kind: 'uups' });
@@ -27,9 +27,9 @@ async function deployWithProxy(contract) {
 }
 
 async function main() {
-  metadata = await deploy("EditionMetadata");
-  editions = await deployWithArgs("MintableEditions", metadata);
-  factory = await deployWithArgs("MintableEditionsFactory", editions);
+  const metadata = await deploy("EditionMetadata");
+  const editions = await deployWithArgs("MintableEditions", metadata);
+  const factory = await deployWithArgs("MintableEditionsFactory", editions);
 }
 
 main()
