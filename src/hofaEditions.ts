bb776c7b91ddcd1e0b6e9b28adeb3139a3849f26
Emanueleeee <lelex.ea.ea@gmail.com>
2021-12-06 14:15:05 +0100

    //function

--- src/hofaEditions.ts
+++ src/hofaEditions.ts
@@ -1,35 +1,35 @@
 const { expect } = require("chai");
-const { ethers, deployments } = require("hardhat");
-
+const { ethers,  } = require("hardhat");
+ 
 import "@nomiclabs/hardhat-ethers";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { values } from "underscore";
+
+
 import {
   MintableEditionsFactory,
   MintableEditions,
   EditionMetadata
 } from "../typechain";
-import { EditionSoldEvent } from "../typechain/MintableEditions";
 
 
 
 
 
 
-export class HofaEditions{
-public Emetadata:EditionMetadata
-public Editions:MintableEditions
-public  Factory: MintableEditionsFactory
 
+  export class hofaEditions{
+ public Emetadata!: EditionMetadata 
+public Editions!:  MintableEditions 
+    public Factory!: MintableEditionsFactory
+        
+      
 
-  
 public async purchase(editionsId:number ): Promise<number> {
    const factory= await  ethers.getContractAt("MintableEditionsFactory", this.Factory.address) as  MintableEditionsFactory;
    const editionsAddress = await factory.get(editionsId);
    const editions = await ethers.getContractAt("MintableEditions", editionsAddress);
    const price = await editions.price();
    expect(price).to.not.be.equal("0x0");
- return editions.purchase(values);
+ return editions.purchase();
      
     
     
@@ -50,12 +50,13 @@ public async mintMultiple(editionsId:number,count:number):Promise<number>{
     const editionsMultiple= await ethers.getContractAt("MintableEditions", editionsAddress);
     return editionsMultiple.mintAndTransfer(count);
 }
-public async mintAndTransfer(editionsId:number, recipients:Array<['address']>, count:number):Promise<number>{
-    count=1
+public async mintAndTransfer(editionsId:number, recipients:Array<['address']>, count:number=1):Promise<number>{
+    count = count;
     const factory= await ethers.getContractAt("MintableEditionsFactory", this.Factory.address) as MintableEditionsFactory;
     const editionsAddress= await factory.get(editionsId);
     const editionsMultiple= await ethers.getContractAt("MintableEditions", editionsAddress);
     return editionsMultiple.mintAndTransfer(recipients);
     
 
+}
 }
