66da8b143c91525adf648f1436b258dc3c5818ed
Roberto Lo Giacco <rlogiacco@gmail.com>
2021-12-08 01:17:39 +0100

    partially fixed method create

--- src/hofaEditions1.ts
+++ src/hofaEditions1.ts
@@ -1,35 +1,27 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
- 
+import { Provider } from '@ethersproject/providers'
+import { Signer } from '@ethersproject/abstract-signer'
+
 import "@nomiclabs/hardhat-ethers";
 import { Wallet } from '@ethersproject/wallet'
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 
-import {
-	MintableEditionsFactory,
-	MintableEditions,
-	EditionMetadata,
-	
-} from "../typechain";
-
+import { rejects } from 'assert';
+import { 
+	MintableEditionsFactory, MintableEditionsFactory__factory, 
+	MintableEditions, MintableEditions__factory } from '../typechain';
 
 export class hofaEditions {
+	public signerOrProvider: Signer | Provider;
+	public factory: MintableEditionsFactory;
 
-	public MintableEdition!: MintableEditions
-	public Factory!: MintableEditionsFactory
-	public shareholder!: SignerWithAddress;
-
-	constructor (
-		factoryAddr?: string
-	) {
-		if (!factoryAddr) {
+	constructor (signerOrProvider: Signer | Provider, factoryAddress?: string) {
+		this.signerOrProvider = signerOrProvider;
+		if (factoryAddress) {
 			//load Factory contract
-			this.Factory = ethers.getContractAt("MintableEditionsFactory", factoryAddr) as MintableEditionsFactory;
-			this.MintableEdition = ethers.getContractAt("MintableEditions", this.Factory);
+			this.factory = MintableEditionsFactory__factory.connect(factoryAddress as string, signerOrProvider);
 		} else {
-			this.Factory = ethers.getContractAt("MintableEditionsFactory") as MintableEditionsFactory;
-			this.MintableEdition = ethers.getContractAt("MintableEditions", this.Factory);
+			this.factory = MintableEditionsFactory__factory.connect("0x0000", signerOrProvider);
 		}
 	}
 	// create functions
@@ -37,34 +29,39 @@ export class hofaEditions {
 	// create a MintableEdition
 	// @param address // The creator address for Edition create (I suppose)
 	// public async create(name:string,symbol:string,description:string,contentUrl:string,contentHash:string,thumbnailUrl:string,size:number,royalties:number,shares: { holder: string; bps: number }[]):Promise<MintableEditions>{
-	public async create(name:string,symbol:string,description:string,contentUrl:string,contentHash:string,thumbnailUrl:string,size:number,royalties:number,shares: { holder: string; bps: number }[]){
-		const factory = (await ethers.getContractAt("MintableEditionsFactory")) as MintableEditionsFactory;
-	  
-		// const editionsAddress = await factory.create(
-		factory.create(
-		name,
-		symbol,
-		description,
-		contentUrl,
-		contentHash,
-		thumbnailUrl,
-		size,
-		royalties,
-		shares)
+	public async create(
+		name:string,
+		symbol:string,
+		description:string,
+		contentUrl:string,
+		contentHash:string,
+		thumbnailUrl:string,
+		size:number,
+		royalties:number,
+		shares: { holder: string; bps: number }[]): Promise<MintableEditions> {
+			
+			const callerAddress = await (this.signerOrProvider as Signer).getAddress();
+			
+			await this.factory.create(name, symbol, description, contentUrl, contentHash, thumbnailUrl, size, royalties, shares);
+			const filterCreator = await this.factory.filters.CreatedEditions(null, callerAddress , null, null, null);
+			
+			this.factory.on(filterCreator, (a,b,c,d,e,f) => {
+				console.log("a: " + a);
+				console.log("b: " + b);
+				console.log("c: " + c);
+				console.log("d: " + d);
+				console.log("e: " + e);
+				console.log("f: " + f);
+			})
+			const editionsAddress = await this.factory.get(1);
 
-		// Verify creation
-		/*
-		const ver = factory.instances();
-		const editions = await ethers.getContractAt("MintableEditions", editionsAddress)as MintableEditions
-		this.Factory = factory
-		this.MintableEdition = editions
-		return editions
-		*/
+			return new Promise((resolve) => { 
+				resolve(MintableEditions__factory.connect(editionsAddress, this.signerOrProvider)) 
+			});
 	  }
 
 	public async get(id: number): Promise<string>{
-		const factory = (await ethers.getContractAt("MintableEditionsFactory")) as MintableEditionsFactory;
-		return factory.get(id)
+		return this.factory.get(id)
 	}
 
 	/*
--- test/hofaclass.ts
+++ test/hofaclass.ts
@@ -5,22 +5,42 @@ const { ethers, deployments } = require("hardhat");
 import "@nomiclabs/hardhat-ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { hofaEditions } from "../src/hofaEditions1"
-import { MintableEditions, MintableEditionsFactory } from "../typechain"
+import { MintableEditions } from "../typechain"
 
 
-describe('Hofaclass', () => {
+// se metto only, questo e' l'unica suite di test eseguita
+describe.only('Hofaclass', () => {
+	let hofa: hofaEditions;
+	let artist: SignerWithAddress;
+
+	// questo codice viene eseguito prima di ogni test
+	beforeEach(async () => {
+		[artist] = await ethers.getSigners(); // recupero un wallet con cui eseguire il test
+		hofa = new hofaEditions(artist, (await deployments.get("MintableEditionsFactory")).address); // recupero l'indirizzo della factory deployata da --deploy-fixture
+	})
+
 	describe("#constructor", () => {
 		let test: string
 	})
 
 	describe("create function", () => {
-		let test = new hofaEditions()
-
 		it("should create a mintableEditions", async function() {
-			test.create("Roberto Lo Giacco", "", "", "https://ipfs.io/ipfs/bafybeib52yyp5jm2vwifd65mv3fdmno6dazwzyotdklpyq2sv6g2ajlgxu", "0x04db57416b770a06b3b2123531e68d67e9d96872f453fa77bc413e9e53fc1bfc", "https://i.imgur.com/FjT55Ou.jpg", 1000, 250, [])
-			// const editions = (await ethers.getContractAt("MintableEditions", await test.get(0))) as MintableEditions;
-			// expect(await editions.contentHash()).to.be.equal("0x04db57416b770a06b3b2123531e68d67e9d96872f453fa77bc413e9e53fc1bfc");
-			
+			// se faccio questo
+			const editions = await hofa.create(
+				"Roberto Lo Giacco", 
+				"", 
+				"", 
+				"https://ipfs.io/ipfs/bafybeib52yyp5jm2vwifd65mv3fdmno6dazwzyotdklpyq2sv6g2ajlgxu", 
+				"0x04db57416b770a06b3b2123531e68d67e9d96872f453fa77bc413e9e53fc1bfc", 
+				"https://i.imgur.com/FjT55Ou.jpg", 
+				1000, 
+				250, 
+				[]);
+			editions.connect(artist);
+			// allora deve succedere questo
+			console.log("editions:" + editions.address);
+			expect(await editions.connect(artist).name()).to.be.equal("Roberto Lo Giacco");
+			expect(await editions.connect(artist).contentHash()).to.be.equal("0x04db57416b770a06b3b2123531e68d67e9d96872f453fa77bc413e9e53fc1bfc");
 		})
 	})
 
