5fe3f55794f220cb65d98e0c8f5c7c7f7e5a2c67
Narthar <narthar@gmail.com>
2021-12-08 18:21:53 +0100

    [UPD] Adde renamed test file

---
+++ test/HofaMeNFT.test.ts
@@ -0,0 +1,46 @@
+require("chai").use(require('chai-as-promised'));
+const { expect } = require("chai");
+const { ethers, deployments } = require("hardhat");
+
+import "@nomiclabs/hardhat-ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { HofaMeNFT, MeNFTInfo } from "../src/HofaMeNFT"
+import { MintableEditions } from "../typechain"
+
+
+describe.only('On HofaMeNFT', () => {
+	let hofa: HofaMeNFT;
+	let artist: SignerWithAddress;
+	let curator: SignerWithAddress;
+	let shareholder: SignerWithAddress;
+
+	beforeEach(async () => {
+		[artist, shareholder, curator] = await ethers.getSigners(); // recupero un wallet con cui eseguire il test
+		hofa = new HofaMeNFT(artist, (await deployments.get("MintableEditionsFactory")).address); // recupero l'indirizzo della factory deployata da --deploy-fixture
+	})
+
+	describe("#constructor", () => {
+		let test: string
+	})
+
+	describe("the create function", () => {
+		it("should create a MeNFT", async function() {
+			// given
+			const info:MeNFTInfo = {
+				name: "Emanuele",
+				symbol: "LELE",
+				description: "My very first MeNFT",
+				contentUrl:"https://ipfs.io/ipfs/bafybeib52yyp5jm2vwifd65mv3fdmno6dazwzyotdklpyq2sv6g2ajlgxu",
+				size: 1000,
+				royalties: 250,
+				shares: [{ holder:curator.address, bps:100 }],
+			}
+			// when
+			const editions = await hofa.create(info);
+			editions.connect(artist);
+			// then
+			expect(await editions.connect(artist).name()).to.be.equal("Emanuele");
+			expect(await editions.connect(artist).contentHash()).to.be.equal("0x5f9fd2ab1432ad0f45e1ee8f789a37ea6186cc408763bb9bd93055a7c7c2b2ca");
+		})
+	})
+});
