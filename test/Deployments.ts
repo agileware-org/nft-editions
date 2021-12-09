d49e1a6bae9d6d7dfc82f25258f33173b1c205de
Roberto Lo Giacco <rlogiacco@gmail.com>
2021-12-09 19:46:19 +0100

    Revert "[UPD] Removed others test files"
    This reverts commit a96db38e17e78b91a4b105db60a09e88c9caf459.

---
+++ test/Deployments.ts
@@ -0,0 +1,30 @@
+const { expect } = require("chai");
+const { ethers, deployments } = require("hardhat");
+
+import "@nomiclabs/hardhat-ethers";
+
+describe("Deployments", function () {
+
+  it("Should deploy MintableEditionsFactory", async function () {
+    const EditionsMetadataHelper = await ethers.getContractFactory("EditionsMetadataHelper");
+    const metadata = await EditionsMetadataHelper.deploy();
+
+    const MintableEditions = await ethers.getContractFactory("MintableEditions");
+    const editionsTemplate = await MintableEditions.deploy(metadata.address);
+
+    const MintableEditionsFactory = await ethers.getContractFactory("MintableEditionsFactory");
+    const factory = await MintableEditionsFactory.deploy(editionsTemplate.address);
+  });
+
+  it("Should deploy SplitterFactory contracts", async function () {
+    const PushSplitter = await ethers.getContractFactory("PushSplitter");
+    const pushSplitter = await PushSplitter.deploy();
+
+    const ShakeableSplitter = await ethers.getContractFactory("ShakeableSplitter");
+    const shakeableSplitter = await ShakeableSplitter.deploy();
+
+    const SplitterFactory = await ethers.getContractFactory("SplitterFactory");
+    const pushSplitterFactory = await SplitterFactory.deploy(pushSplitter.address);
+    const shakeableSplitterFactory = await SplitterFactory.deploy(shakeableSplitter.address);
+  });
+});
