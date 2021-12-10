725e1997a7614baaab675f7c089e35e300f501c4
Roberto Lo Giacco <rlogiacco@gmail.com>
2021-12-11 00:18:05 +0100

    moves addresses.json

--- scripts/verify.ts
+++ scripts/verify.ts
@@ -18,7 +18,7 @@ async function verify(contract:string, args: any[]) {
 }
 
 async function main() {
-  const addresses = JSON.parse(readFileSync('./addresses.json', 'utf-8'));
+  const addresses = JSON.parse(readFileSync('./src/addresses.json', 'utf-8'));
   addresses[await getChainId()] = contracts;
   
   await verify('EditionsMetadataHelper', []);
@@ -29,7 +29,7 @@ async function main() {
   await verify('SplitterFactory', [await (await get('PushSplitter')).address]);
   await verify('SplitterFactory', [await (await get('ShakeableSplitter')).address]);
 
-  writeFileSync('./addresses.json', JSON.stringify(addresses, null, 2), {encoding: 'utf-8'});
+  writeFileSync('./src/addresses.json', JSON.stringify(addresses, null, 2), {encoding: 'utf-8'});
 }
 
 main()
