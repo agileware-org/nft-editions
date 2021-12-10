a3477105c40b562f95dca8fb860f253019fbe4fc
Roberto Lo Giacco <rlogiacco@gmail.com>
2021-12-10 22:19:41 +0100

    lowers solidity compiler version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,7 +27,7 @@ const config: HardhatUserConfig = {
   },
   solidity: {
     compilers: [{ 
-      version: "0.8.10", 
+      version: "0.8.9", 
       settings: {
         optimizer: {
           runs: 200000,
